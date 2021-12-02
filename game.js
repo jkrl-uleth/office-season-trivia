71aa05d54048ff89544f51b33b0c52b0d867da12
Jesse Kearl <jesse.kearl@Jesses-MacBook-Air.local>
2021-12-02 09:38:11 -0700

    more code

--- game.js
+++ game.js
@@ -4,24 +4,38 @@
         { audio: "some/path", character: "michael", season: 4, score: 250 },
         { audio: "some/path", character: "michael", season: 4, score: 250 },
     ]
-    var possibleQuestionScores = [250, 500, 1000]
+    var scores = [250, 500, 1000]
     var score = 0
-    var answerCounter = 0
+    var answerCounter = 0 // game over when 12 questions have been answered
 
     function selectQuestions() {
         var selectedQuestions = []
 
-        for (var score of possibleQuestionScores) {
+        for (var score of scores) {
+            // filter all questions of certain score, then randomly select 4 to use for the game
             var scoreQuestions = questions.filter(e => e.score === score)
-            var scoreSelectedQuestions = _.sampleSize(scoreQuestions, 4)
-            selectedQuestions.push(scoreSelectedQuestions)
+            var scoreQuestionsSample = _.sampleSize(scoreQuestions, 4)
+            selectedQuestions.push(scoreQuestionsSample)
         }
 
         return selectedQuestions
     }
 
+    function questionDiv(question) {
+        return `<div class="active-question">${question.score}</div>`
+    }
+
+    function renderQuestions(selectedQuestions) {
+        board = document.getElementById("game-board")
+        board.innerHTML = ""
+        for (var question in selectedQuestions) {
+            board.appendChild(questionDiv(question))
+        }
+    }
+
     function initGame() {
         var selectedQuestions = selectQuestions()
+        renderQuestions(selectedQuestions)
     }
 
     initGame()
