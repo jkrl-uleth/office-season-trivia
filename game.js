42bcefcf8be677edb45f5bed803adacd4285edc4
Jesse Kearl <jesse.kearl@Jesses-MacBook-Air.local>
2021-12-03 13:10:32 -0700

    in a good spot

--- game.js
+++ game.js
@@ -24,7 +24,7 @@
                 "5_stay-calm",
                 "5_no-god-no",
             ],
-            points: 250,
+            points: 50,
             sampleSize: 4,
         },
         {
@@ -33,18 +33,17 @@
                 "2_kill-myself",
                 "2_the-way-you-are",
                 "3_am-i-a-hero",
-                "3_dont-be-an-idiot",
                 "3_fool-me-once",
                 "4_creed-debt",
                 "4_hemorrhoids",
                 "4_superstitious",
-                "5_start_a_sentence",
+                "5_start-a-sentence",
                 "5_turntables",
                 "6_hardcore-parkour",
                 "6_shoot-toby-twice",
                 "9_good-old-days",
             ],
-            points: 500,
+            points: 100,
             sampleSize: 6,
         },
         {
@@ -60,46 +59,109 @@
                 "4_need-to-be-liked",
                 "5_no-idea-what-to-do",
             ],
-            points: 1000,
+            points: 250,
             sampleSize: 2,
         },
     ]
-    var score = 0 // cumulative game score
     var answered = 0 // game ends when 12 questions have been answered
+    var activeClip = null // data for currently selected clip (season, file, points)
+    var score = 0 // cumulative game score
+    var correct = new Audio("audio/right-answer.mp3")
+    var incorrect = new Audio("audio/wrong-answer.mp3")
+
+    function answerHandler(season) {
+        var clip = document.getElementById(activeClip.file)
+        activeClip.audio.pause()
+        if (activeClip.season === season) {
+            score += activeClip.points
+            document.getElementById("score").innerHTML = `Score: ${score}`
+            correct.play()
+        } else {
+            incorrect.play()
+        }
+        activeClip = null
+        clip.classList.add("disabled")
+        clip.onclick = null
+        modal.style.display = "none"
+        if (++answered === 12) {
+        }
+    }
+
+    function renderSeasonDiv(season) {
+        var div = document.createElement("div")
+        var span = document.createElement("span")
+        document.getElementById("season-container").appendChild(div)
+        div.appendChild(span)
+        div.classList.add("season")
+        span.innerHTML = season
+        span.classList.add("button-font")
+        div.onclick = function () {
+            answerHandler(season)
+        }
+    }
+
+    function renderModal() {
+        var modal = document.getElementById("modal")
+        modal.addEventListener("click", function (e) {
+            if (e.target.id === this.id) {
+                activeClip.audio.pause()
+                activeClip = null
+                modal.style.display = "none"
+            }
+        })
+
+        for (var i = 1; i < 10; i++) renderSeasonDiv(i)
+    }
 
     function selectClips() {
         var gameClips = []
         for (var tier of data) {
-            var sample = _.sampleSize(tier.clips, tier.sampleSize)
-            for (var clip of sample) {
-                gameClips.push(`${clip}_${tier.points}`)
-            }
+            gameClips.push(_.sampleSize(tier.clips, tier.sampleSize))
         }
         return gameClips
     }
 
-    function clipDiv(clip) {
-        var info = clip.split("_")
-        var clip = document.createElement("div")
-        clip.classList.add("clip")
-        clip.setAttribute("data-season", info[0])
-        clip.setAttribute("data-clip", info[1])
-        clip.setAttribute("data-points", info[2])
-        // todo: add the onclick
+    function getClipDiv(clip, points) {
+        var div = document.createElement("div")
+        var span = document.createElement("span")
+        var modal = document.getElementById("modal")
+        var info = clip.split("_") // season, file
+        div.appendChild(span)
+        div.setAttribute("id", info[1])
+        div.classList.add("clip")
+        span.innerHTML = points
+        span.classList.add("clip-points")
+        // have to use onclick so I can remove the listener in answerHandler
+        div.onclick = function () {
+            var audio = new Audio(`audio/${clip}.mp3`)
+            activeClip = {
+                season: Number(info[0]),
+                file: info[1],
+                points: points,
+                audio: audio,
+            }
+            modal.style.display = "flex"
+            activeClip.audio.play()
+        }
+        return div
     }
 
     function renderClips(gameClips) {
-        board = document.getElementById("board")
-        for (var clip of gameClips) {
-            board.appendChild(clipDiv())
-        }
+        gameClips.forEach((e, i) => {
+            for (var clip of e) {
+                var child = document.getElementById("inner").children[i]
+                child.appendChild(getClipDiv(clip, data[i].points))
+            }
+        })
     }
 
-    function initGame() {
+    function init() {
+        document.getElementById("score").innerHTML = "Score: 0"
+        renderModal()
         var gameClips = selectClips()
         console.log(gameClips)
         renderClips(gameClips)
     }
 
-    initGame()
+    init()
 })()
